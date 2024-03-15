fa9bbcbc71a2e76b213d318283cd2476fe80f7c7
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-03-15 19:12:30 +0530

    image core component done

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,11 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, ImageBackground } from "react-native";
+import logoImg from "./assets/adaptive-icon.png";
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Text>
+      <ImageBackground source={logoImg} style={{ flex: 1 }}>
+        <Text>Image Text</Text>
+      </ImageBackground>
+      <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      <Image
+        source={{ uri: "https://picsum.photos/300" }}
+        style={{ width: 300, height: 300 }}
+      />
+      {/* <Text>
         <Text style={{ color: "white" }}>Hello</Text> World
-      </Text>
+      </Text> */}
       {/* <View
         style={{ width: 200, height: 200, backgroundColor: "lightblue" }}
       ></View>
