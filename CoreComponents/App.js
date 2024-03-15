eef4e72eb9734349c23c2009805fd68403b8ee94
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-03-15 19:38:41 +0530

    button core component done

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,10 +1,23 @@
-import { View, Text, Image, ImageBackground, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  ImageBackground,
+  ScrollView,
+  Button,
+} from "react-native";
 import logoImg from "./assets/adaptive-icon.png";
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <ScrollView>
+      <Button
+        title="Press"
+        onPress={() => console.log("button pressed")}
+        color="midnightblue"
+        disabled
+      />
+      {/* <ScrollView>
         <Image source={logoImg} style={{ width: 300, height: 300 }} />
         <Text>
           Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -18,23 +31,23 @@ export default function App() {
           including versions of Lorem Ipsum.
         </Text>
         <Image source={logoImg} style={{ width: 300, height: 300 }} />
-        {/* <ImageBackground source={logoImg} style={{ flex: 1 }}>
-        <Text>Image Text</Text>
-      </ImageBackground> */}
-        {/* <Image
-        source={{ uri: "https://picsum.photos/300" }}
-        style={{ width: 300, height: 300 }}
-      /> */}
-        {/* <Text>
-        <Text style={{ color: "white" }}>Hello</Text> World
-      </Text> */}
-        {/* <View
-        style={{ width: 200, height: 200, backgroundColor: "lightblue" }}
-      ></View>
-      <View
-        style={{ width: 200, height: 200, backgroundColor: "lightgreen" }}
-      ></View> */}
-      </ScrollView>
+        <ImageBackground source={logoImg} style={{ flex: 1 }}>
+          <Text>Image Text</Text>
+        </ImageBackground>
+        <Image
+          source={{ uri: "https://picsum.photos/300" }}
+          style={{ width: 300, height: 300 }}
+        />
+        <Text>
+          <Text style={{ color: "white" }}>Hello</Text> World
+        </Text>
+        <View
+          style={{ width: 200, height: 200, backgroundColor: "lightblue" }}
+        ></View>
+        <View
+          style={{ width: 200, height: 200, backgroundColor: "lightgreen" }}
+        ></View>
+      </ScrollView> */}
     </View>
   );
 }
