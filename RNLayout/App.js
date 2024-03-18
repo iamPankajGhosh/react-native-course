e604b742926f9827a00891892f2f34e02a02c253
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-03-18 13:44:04 +0530

    flexbox done

---
+++ RNLayout/App.js
@@ -0,0 +1,25 @@
+import { StyleSheet, View } from "react-native";
+import Box from "./components/Box";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Box style={{ backgroundColor: "#8e9b00", flex: 1 }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#b65d1f", flex: 3 }}>Box 2</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "#1c4c06" }}>Box 6</Box>
+      <Box style={{ backgroundColor: "#b95f21" }}>Box 7</Box>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 64,
+    borderWidth: 6,
+    borderColor: "red",
+  },
+});
