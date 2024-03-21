b171fa70a102c03a89745de3a0516aee950005d3
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-03-21 15:51:31 +0530

    flatlist component done

---
+++ RNLists/App.js
@@ -0,0 +1,75 @@
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  SafeAreaView,
+  StatusBar,
+  FlatList,
+} from "react-native";
+import pokemonList from "./data.json";
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* <ScrollView style={styles.scrollView}>
+        {pokemonList.map((pokemon) => (
+          <View key={pokemon.id} style={styles.card}>
+            <Text style={styles.cardText}>{pokemon.type}</Text>
+            <Text style={styles.cardText}>{pokemon.name}</Text>
+          </View>
+        ))}
+      </ScrollView> */}
+      <View style={styles.scrollView}>
+        <FlatList
+          data={pokemonList}
+          renderItem={({ item }) => (
+            <View key={item.id} style={styles.card}>
+              <Text style={styles.cardText}>{item.type}</Text>
+              <Text style={styles.cardText}>{item.name}</Text>
+            </View>
+          )}
+          // horizontal
+          keyExtractor={(item) => item.id.toString()}
+          ItemSeparatorComponent={<View style={{ height: 16 }}></View>}
+          ListEmptyComponent={<Text>No items found</Text>}
+          ListHeaderComponent={
+            <Text style={styles.headerText}>Pokemon List</Text>
+          }
+          ListFooterComponent={
+            <Text style={styles.footerText}>End of List</Text>
+          }
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    paddingHorizontal: 20,
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    // marginBottom: 20,
+  },
+  cardText: {
+    fontSize: 30,
+  },
+  headerText: {
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  footerText: {
+    fontSize: 20,
+  },
+});
