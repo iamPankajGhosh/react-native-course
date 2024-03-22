e6a0d4550ba221c0ed623796d8366b404969113c
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-03-22 13:36:24 +0530

    switch core component done

--- RNForms/App.js
+++ RNForms/App.js
@@ -6,10 +6,12 @@ import {
   SafeAreaView,
   StatusBar,
   TextInput,
+  Switch,
 } from "react-native";
 
 export default function App() {
   const [name, setName] = useState("");
+  const [isDarkMode, setIsDarkMode] = useState(false);
   return (
     <SafeAreaView style={styles.container}>
       <TextInput
@@ -28,6 +30,15 @@ export default function App() {
         multiline
       />
       <Text style={styles.text}>My name is {name}</Text>
+      <View style={styles.switchContainer}>
+        <Text style={styles.text}>Dark Mode</Text>
+        <Switch
+          value={isDarkMode}
+          onValueChange={() => setIsDarkMode((prev) => !prev)}
+          trackColor={{ false: "#767577", true: "#81b0ff" }}
+          thumbColor={isDarkMode ? "#f5dd4b" : "#f4f3f4"}
+        />
+      </View>
     </SafeAreaView>
   );
 }
@@ -37,11 +48,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     paddingTop: StatusBar.currentHeight,
+    padding: 10,
   },
   input: {
     height: 40,
-    margin: 12,
     padding: 10,
+    marginBottom: 12,
     borderWidth: 1,
   },
   text: {
@@ -50,5 +62,11 @@ const styles = StyleSheet.create({
   multilineText: {
     height: 100,
     textAlignVertical: "top",
+  },
+  switchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
   },
 });
