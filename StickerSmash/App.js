f4482703200f27af798e68ce7f85facdef0c7fce
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-04-02 01:27:19 +0530

    add image picker

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,12 +1,43 @@
 import { StatusBar } from "expo-status-bar";
-import { Image, StyleSheet, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+
+import Button from "./components/button";
+import ImageViewer from "./components/ImageViewer";
 import PlaceholderImage from "./assets/images/background-image.png";
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  let pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("You did not select any image");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button
+          theme="primary"
+          label="Choose a photo"
+          onPress={pickImageAsync}
+        />
+        <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -27,5 +58,9 @@ const styles = StyleSheet.create({
     width: 320,
     height: 440,
     borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
